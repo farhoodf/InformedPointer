82ab05d64b707a8e49c59e1b823f1b7b6f121b76
Farhood <farhood.farahnak@gmail.com>
2020-02-24 12:24:03 -0500

    adding log

---
+++ log.py
@@ -0,0 +1,40 @@
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import pyplot as plt
+
+
+class AverageMeter(object):
+	"""Computes the average and stores the values"""
+	def __init__(self):
+		self.reset()
+
+	def reset(self):
+		self.values = []
+		self.sum = 0
+		self.count = 0
+
+	def update(self, val):
+		self.values.append(val)
+		self.count += 1
+		self.sum += val
+
+	def extend(self, val):
+		self.values.extend(val)
+		self.count += len(val)
+		self.sum += sum(val)
+
+	def average(self):
+		return self.sum / self.count
+
+	def last(self):
+		if len(self.values) > 0:
+			return self.values[-1]
+		else:
+			return None
+	def save(self, path):
+		with open(path,'w') as f:
+			for i in range(len(self.values)):
+				f.write(str(i+1).zfill(3)+' '+str(self.values[i])+'\n')
+	def draw_fig(self, path):
+		plt.plot(self.values)
+		plt.savefig(path)
