d6733873196421075b93624907e8541eb008976a
Farhood <farhood.farahnak@gmail.com>
2020-02-23 20:03:16 -0500

    add elmo model

--- data.py
+++ data.py
@@ -11,40 +11,62 @@ import re
 
 from transformers import BertTokenizer
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+from allennlp.modules.elmo import batch_to_ids
 
 
-class SIND(Dataset):
+class AbstractData(Dataset):
+	"""docstring for AbstractData"""
+	def __init__(self,):
+		super(AbstractData, self).__init__()
+	
+	def __len__(self):
+		return len(self.data)
+	
+	def __getitem__(self, idx):
+		return self.data[idx]
+
+	def tokenize(self, tokenizer):
+		for i in range(len(self.data)):
+			self.data[i]['s_len'] = []
+			for j in range(self.data[i]['p_len']):
+
+				self.data[i]['text'][j] = tokenizer(self.data[i]['text'][j])
+				self.data[i]['s_len'].append(len(self.data[i]['text'][-1]))
+	
+	def vectorize_dic(self, w_to_id):
+		for i in range(self.data):
+			self.data[i]['vectorized'] = []
+			for j in range(self.data[i]['p_len']):
+				self.data[i]['vectorized'].append([])
+				for k in range(self.data[i]['s_len'][j]):
+					self.data[i]['vectorized'][j].append(w_to_id[self.data[i]['text'][j][k]])
+
+class SIND(AbstractData):
 	"""docstring for SIND"""
-	def __init__(self, path):
+	def __init__(self, path, tokenizer=None):
 		super(SIND, self).__init__()
 		self.data = []
 		self.path = path
 		self.__load_data__()
 
-	def __len__(self):
-		return len(self.data)
-
-	def __getitem__(self, idx):
-		return self.data[idx]
+		if tokenizer is not None:
+			self.tokenize(tokenizer)
 	
 	def __load_data__(self):
 		with open(self.path,'r') as f:
 			raw = json.load(f)
 		for key in raw:
 			raw_sample = raw[key]
-			# sample = [[conf['first_sent']]]+[raw[key][str(i)]['text'] for i in range(len(raw_sample))]+[[conf['last_sent']]]
-			sample = [[conf['first_sent']]]+[raw[key][str(i)]['original_text'] for i in range(len(raw_sample))]#+[[conf['last_sent']]]
+			sample = [conf['first_sent']]+[raw[key][str(i)]['original_text'] for i in range(len(raw_sample))]#+[[conf['last_sent']]]
 
-
-
-			# sent_len = []
-			# for sent in sample:
-			# 	sent_len.append(len(sent))
 			labels = list(range(len(sample)))
 			self.data.append({'text':sample,'labels':labels,'p_len':len(labels)})
 
+			# self.data = self.data[:1000]
 
-class Synthetic(Dataset):
+
+
+class Synthetic(AbstractData):
 	"""docstring for Synthetic"""
 	def __init__(self, path):
 		super(Synthetic, self).__init__()
@@ -52,11 +74,6 @@ class Synthetic(Dataset):
 		self.path = path
 		self.__load_data__()
 
-	def __len__(self):
-		return len(self.data)
-
-	def __getitem__(self, idx):
-		return self.data[idx]
 	
 	def __load_data__(self):
 		with open(self.path,'r') as f:
@@ -121,6 +138,49 @@ def bert_batchify(batch):
 
 		for j in range(p_lens[i]):
 			padded_data[i,j,:s_lengths[i][j]] = torch.tensor(data[i][j])
+			padded_lengths[i,j] = s_lengths[i][j]
+
+
+	return {'data':padded_data,'s_lengths':padded_lengths, 'labels': padded_labels,
+			'p_lengths':torch.tensor(p_lens), 'text':text}
+
+def elmo_batchify(batch):
+
+	batch_size = len(batch)
+	data = []
+	labels = []
+	s_lengths = []
+	p_lens = []
+	max_sent_len = []
+	text = []
+
+	for sample in batch:
+		# d = []
+		# for sent in sample['text']:
+		# 	d.append(batch_to_ids(sent))
+		d = batch_to_ids(sample['text'])
+		text.append(sample['text'])
+		s_lengths.append(sample['s_len'])
+		data.append(d)
+		labels.append(sample['labels'])
+		
+		p_lens.append(d.shape[0])
+		max_sent_len.append(d.shape[1])
+	p_len = max(p_lens)
+	s_len = max(max_sent_len)
+
+	padded_data = torch.zeros((batch_size,p_len,s_len,50),dtype=torch.long)
+	padded_lengths = torch.ones((batch_size,p_len),dtype=torch.long)
+	padded_labels = torch.zeros((batch_size,p_len),dtype=torch.long)
+	for i in range(batch_size):
+		shuffled_indices = torch.randperm(p_lens[i])
+		# data[i] = [data[i][k] for k in shuffled_indices]
+		s_lengths[i] = [s_lengths[i][k] for k in shuffled_indices]
+		padded_labels[i,:p_lens[i]] = torch.argsort(shuffled_indices)
+		text[i] = [text[i][k] for k in shuffled_indices]
+
+		for j in range(p_lens[i]):
+			padded_data[i,j,:max_sent_len[i]] = data[i][shuffled_indices[j]]
 			padded_lengths[i,j] = s_lengths[i][j]
 
 
--- models.py
+++ models.py
@@ -2,6 +2,68 @@ import torch
 import torch.nn as nn
 from transformers import DistilBertModel, BertModel
 import torch.nn.functional as F
+from torch.nn import init
+from torch.nn.parameter import Parameter
+from allennlp.modules.elmo import Elmo, batch_to_ids
+
+options_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x1024_128_2048cnn_1xhighway/elmo_2x1024_128_2048cnn_1xhighway_options.json" 
+weight_file = "https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x1024_128_2048cnn_1xhighway/elmo_2x1024_128_2048cnn_1xhighway_weights.hdf5"
+
+
+def getmask_hugging(s_lengths, batch_size, s_len):
+	"""attention_mask (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`):
+		Mask to avoid performing attention on padding token indices.
+		Mask values selected in ``[0, 1]``:
+		``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens."""
+	mask=torch.zeros((batch_size,s_len+1),dtype=torch.bool,device=s_lengths.device,requires_grad=False)
+
+	mask[torch.arange(batch_size),s_lengths] = 1
+	mask = ~mask.cumsum(dim=1).bool().to(device=s_lengths.device)
+	return mask[:,:-1]
+
+class FromELMO(nn.Module):
+	"""docstring for FromELMO"""
+	def __init__(self):
+		super(FromELMO, self).__init__()
+		self.dropout_value = 0.3
+
+		# self.word_encoder = BertModel.from_pretrained('bert-base-uncased')
+		self.word_encoder = Elmo(options_file, weight_file, 1, dropout=self.dropout_value)
+		self.sentence_encoder = SentenceEncoder(256)
+		self.parag_encoder = ParagEncoder()
+		self.pointer = InformedPointer(embedding_size=256)
+		
+		self.word_drop = nn.Dropout(self.dropout_value)
+		self.sent_drop = nn.Dropout(self.dropout_value)
+	
+	def forward(self, x, s_lengths, p_lengths, labels=None):
+		batch_size, p_len, s_len,_ = x.size()
+
+		#changing view
+		# x_ = x.view(p_len*batch_size, s_len)
+		# s_lengths_ = s_lengths.view(p_len*batch_size)
+		# sent_mask = getmask_hugging(s_lengths_, batch_size*p_len, s_len)
+		
+		elmo_out = self.word_encoder(x)
+		word_embedded = elmo_out['elmo_representations'][0].view(p_len*batch_size, s_len,-1)
+
+		mask = elmo_out['mask'].bool()
+		word_embedded = self.word_drop(word_embedded)
+
+		# sent_embedded = sent_embedded.view(batch_size, p_len, -1)
+		sent_embedded = self.sentence_encoder(word_embedded, mask=mask.view(p_len*batch_size, s_len))
+		sent_embedded = self.sent_drop(sent_embedded)
+
+
+		word_embedded = word_embedded.view(batch_size, p_len, s_len, -1)
+		sent_embedded = sent_embedded.view(batch_size, p_len, -1)
+		
+		parag_embedded = self.parag_encoder(sent_embedded)
+
+
+		out = self.pointer(parag_embedded, sent_embedded, word_embedded, mask, labels)
+
+		return out
 
 class FromBert(nn.Module):
 	"""docstring for FromBert"""
@@ -9,17 +71,23 @@ class FromBert(nn.Module):
 		super(FromBert, self).__init__()
 		self.dropout_value = 0.3
 
-		self.bert = BertModel.from_pretrained('bert-base-uncased')
+		self.word_encoder = BertModel.from_pretrained('bert-base-uncased')
+		# self.sentence_embedding = Attention(768, 768, internal_dim=None, need_attn=True)
+		# self.sentence_embedding_query = nn.Linear(1,768, bias=False)
+		self.sentence_encoder = SentenceEncoder(768)
+		self.parag_encoder = ParagEncoder()
 		self.pointer = InformedPointer()
+		
 		self.word_drop = nn.Dropout(self.dropout_value)
 		self.sent_drop = nn.Dropout(self.dropout_value)
+	
 	def forward(self, x, s_lengths, p_lengths, labels=None):
 		batch_size, p_len, s_len = x.size()
 
 		#changing view
 		x_ = x.view(p_len*batch_size, s_len)
 		s_lengths_ = s_lengths.view(p_len*batch_size)
-		sent_mask = self.getmask(s_lengths_, batch_size*p_len, s_len)
+		sent_mask = getmask_hugging(s_lengths_, batch_size*p_len, s_len)
 		# sent_mask = None
 
 		# word_embedded, sent_embedded = self.bert(x_, attention_mask = sent_mask)
@@ -27,45 +95,98 @@ class FromBert(nn.Module):
 		# word_embedded: (bs*p_len, s_len, 768)
 		# sent_embedded: (bs*p_len, 768)
 		
-		word_embedded, _ = self.bert(x_, attention_mask = sent_mask)
+		word_embedded, _ = self.word_encoder(x_, attention_mask = sent_mask)
 		word_embedded = self.word_drop(word_embedded)
-		word_embedded = word_embedded.view(batch_size, p_len, s_len, -1)
-		
+
 		# sent_embedded = sent_embedded.view(batch_size, p_len, -1)
-		sent_embedded = self.sentence_embedding(word_embedded)
+		sent_embedded = self.sentence_encoder(word_embedded, mask=sent_mask)
+		sent_embedded = self.sent_drop(sent_embedded)
+
+
+		word_embedded = word_embedded.view(batch_size, p_len, s_len, -1)
+		sent_embedded = sent_embedded.view(batch_size, p_len, -1)
 		
-		# sent_embedded = self.sent_drop(sent_embedded)
+		
+		
 		#sent_embedded: (bs, p_len, em)
-		# print('word_embedded',word_embedded.shape)
-		# print('sent_embedded',sent_embedded.shape)
+
 		
-		# maybe transformer to get relative embedding
 		
-		parag_embedded = self.get_parag_embedding(sent_embedded)
-		# print('parag_embedded',parag_embedded.shape)
+		parag_embedded = self.parag_encoder(sent_embedded)
 		# parag_embedded: (bs, em)
 
 		out = self.pointer(parag_embedded, sent_embedded, word_embedded, sent_mask, labels)
 
 		return out
-	def get_parag_embedding(self, sent_embedded):
-		return sent_embedded.sum(1)
 
-	def sentence_embedding(self, word_embedded):
-		return word_embedded.sum(dim=2)
+
+	# def get_sentence_embedding(self, word_embedded, mask):
+	# 	# return word_embedded.sum(dim=2)
+	# 	ones = torch.ones((word_embedded.shape[0],1),device=word_embedded.device)
+	# 	q = self.sentence_embedding_query(ones)
+	# 	_, sent_embedded = self.sentence_embedding(q, word_embedded, mask=~mask)
+	# 	return sent_embedded
 	
-	def getmask(self,s_lengths, batch_size, s_len):
-		"""attention_mask (:obj:`torch.FloatTensor` of shape :obj:`(batch_size, sequence_length)`, `optional`, defaults to :obj:`None`):
-			Mask to avoid performing attention on padding token indices.
-			Mask values selected in ``[0, 1]``:
-			``1`` for tokens that are NOT MASKED, ``0`` for MASKED tokens."""
-		mask=torch.zeros((batch_size,s_len+1),dtype=torch.bool,device=s_lengths.device,requires_grad=False)
-		# print('mask',mask.shape)
-		# print(s_lengths)
-		mask[torch.arange(batch_size),s_lengths] = 1
-		mask = ~mask.cumsum(dim=1).bool().to(device=s_lengths.device)
-		return mask[:,:-1]
 
+
+class ParagEncoder(nn.Module):
+	"""docstring for ParagEncoder"""
+	def __init__(self):
+		super(ParagEncoder, self).__init__()
+		
+	def forward(self, sent_embedded):
+		return sent_embedded.sum(1)
+		
+
+class SentenceEncoder(nn.Module):
+	"""docstring for SentenceEncoder"""
+	def __init__(self, embedding_size ):
+		super(SentenceEncoder, self).__init__()
+		
+		self.attn = Attention(embedding_size, embedding_size, internal_dim=None, need_attn=True)
+		self.query = nn.Linear(1,embedding_size, bias=False)
+	
+	def forward(self, word_embedded, mask):
+		ones = torch.ones((word_embedded.shape[0],1),device=word_embedded.device)
+		q = self.query(ones)
+		_, sent_embedded = self.attn(q, word_embedded, mask=~mask)
+		return sent_embedded
+		
+# class RNNEncoder(nn.Module):
+# 	"""docstring for RNNEncoder"""
+# 	def __init__(self, n_token, embedding_size, hidden_size, n_layers=1, rnn_type='LSTM', bidirectional=True, dropout=0.3):
+# 		super(RNNEncoder, self).__init__()
+# 		self.rnn_type = rnn_type
+# 		self.n_token = n_token
+# 		self.embedding_size = embedding_size
+# 		self.hidden_size = hidden_size
+# 		self.n_layers = n_layers
+# 		self.dropout = dropout
+# 		self.bidirectional = bidirectional
+# 		self.batch_index = 1 # 0 if batch_first
+		
+# 		self.rnn = getattr(nn, self.rnn_type)(self.input_size, self.hidden_size, self.n_layers, 
+# 			dropout=self.dropout, bidirectional=self.bidirectional)
+		
+# 		self.rnn.apply(init_weights)
+		
+
+# 	def forward(self, inputs, hiddens=None, lengths=None):#, enforce_sorted=True):
+# 		# lengths = None
+# 		bs = inputs.shape[1]
+# 		if lengths is not None:
+# 			inputs = torch.nn.utils.rnn.pack_padded_sequence(inputs, lengths, enforce_sorted=False)
+		
+# 		outputs, hiddens = self.rnn(inputs, hiddens)
+
+# 		if lengths is not None:
+# 			outputs, _ = torch.nn.utils.rnn.pad_packed_sequence(outputs)
+
+
+# 		return outputs
+
+		
+	
 class InformedPointer(nn.Module):
 	"""docstring for InformedPointer
 
@@ -77,14 +198,16 @@ class InformedPointer(nn.Module):
 	def __init__(self, embedding_size = 768):
 		super(InformedPointer, self).__init__()
 		self.rnn_type = 'LSTM'
-		self.embedding_size = 768
+		self.embedding_size = embedding_size
+		self.rnn_hidden_size = 512
 		self.n_layers = 1
 		self.dropout_value = 0.3
 
 
 		self.rnn = getattr(nn, self.rnn_type)(self.embedding_size, self.embedding_size, self.n_layers, 
 			dropout=self.dropout_value, bidirectional=False)
-		self.informed_values = Attention(embedding_size, embedding_size)
+		self.informed_attn = Attention(embedding_size, embedding_size)
+		self.informed_query = Attention(embedding_size, embedding_size)
 		self.pointer = Attention(embedding_size,embedding_size,need_attn=False)
 	
 
@@ -106,23 +229,28 @@ class InformedPointer(nn.Module):
 			# get informred keys
 			q = hidden[0][-1].unsqueeze(1).repeat(1,p_len,1).view(batch_size*p_len,self.embedding_size)
 			s = word_embedded.view(batch_size*p_len,s_len,self.embedding_size)
-
-			_, informred = self.informed_values(q, s) #hidden or selected?
+			_, informred = self.informed_attn(q, s) #hidden or selected?
 			informred = informred.view(batch_size,p_len,self.embedding_size)
+			informred = informred + sent_embedded
 
 
 		# 	# point to the next index
 			attn_weights = self.pointer(hidden[0][-1], informred, mask=par_mask)[0]
 			# attn_weights = self.pointer(hidden[0][-1], sent_embedded, mask=par_mask)[0]
 
-
+			
 
 			if do_tf:
 				index = labels[:,i]
 			else:
 				index = attn_weights.argmax(1)
 
-			selected = sent_embedded[torch.arange(batch_size),index].unsqueeze(0)
+			s = word_embedded[torch.arange(batch_size),index]
+			q = hidden[0][-1]
+
+			_, informed_query = self.informed_query(q,s)
+			selected = sent_embedded[torch.arange(batch_size),index] + informed_query
+			selected = selected.unsqueeze(0)
 
 			par_mask = par_mask.clone().detach()
 			par_mask[torch.arange(batch_size),index] = 1
@@ -154,21 +282,26 @@ class Attention(nn.Module):
 		
 		if internal_dim == None:
 			internal_dim = v_dim
+		self.dropout_value = 0.2
+
 		self.need_attn = need_attn
 
 		self.proj_q = nn.Linear(q_dim, internal_dim)
 		self.proj_k = nn.Linear(v_dim, internal_dim)
 		self.proj_e = nn.Linear(2*internal_dim,1)
 
-
+		self.drop_q = nn.Dropout(self.dropout_value)
+		self.drop_k = nn.Dropout(self.dropout_value)
 
 	def forward(self, query, states, mask=None):
 		bs,slen,em = states.shape
 		q = F.relu(self.proj_q(query)).unsqueeze(1).repeat(1,slen,1)
+		q = self.drop_q(q)
 		k = F.relu(self.proj_k(states))
+		k = self.drop_k(k)
+
 		energy = self.proj_e(torch.tanh(torch.cat((q,k),-1))).squeeze(2)
 		# if not self.need_attn:
-		# 	print('energy',energy.shape)
 		if mask is not None:
 			energy.masked_fill_(mask,1e-45)
 		# mask can be applied here to put very low value for energy
@@ -193,16 +326,10 @@ class Attention(nn.Module):
 	# 	# q = F.relu(self.proj_q(query)).unsqueeze(1).repeat(1,slen,1)
 	# 	k = self.proj_k(states)
 
-	# 	print('q',q.shape)
-	# 	print('k',k.shape)
-	# 	print('q',q)
 	# 	q = q.unsqueeze(1).unsqueeze(1).repeat(1,p_len,s_len,1)
-	# 	print(q.shape)
-	# 	print(k)
 
 	# 	energy = self.proj_e(torch.tanh(torch.cat((q,k),-1)))
 	# 	attn_weights = F.softmax(energy,dim=1)
-	# 	print('attn_weights',attn_weights.shape)
 
 	# 	out = (attn_weights.squeeze(-1),)
 
